e9928d4d7caa5dd7bd7209be778b7eac2bd7ec89
wincubate <jgh@wincubate.net>
2020-10-30 11:07:00 +0100

    Added two-liner + clean-up of Module 09

--- Design Patterns/Presentations/09 - Repository/Examples/Data/Product.cs
+++ Design Patterns/Presentations/09 - Repository/Examples/Data/Product.cs
@@ -1,6 +1,6 @@
 ﻿namespace Wincubate.RepositoryExamples.Data
 {
-    public class Product : IEntity
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
